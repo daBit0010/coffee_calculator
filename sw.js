ff9c90b3153ef2c242bbd2dae31ee4dae8a39aeb
daBit0010 <damosu02@icloud.com>
2025-09-19 10:23:03 -0600

    Add PWA support with secure service worker

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'coffee-calculator-v1';
+const ASSETS = ['./', './index.html', './app.js', './manifest.webmanifest', './favicon.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  const requestUrl = new URL(request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request, { ignoreSearch: true }).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(request)
+        .then((response) => {
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+          }
+          return response;
+        })
+        .catch(() => {
+          if (request.mode === 'navigate' || request.destination === 'document') {
+            return caches.match('./index.html');
+          }
+          return Response.error();
+        });
+    })
+  );
+});
